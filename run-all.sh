a2e902a4ddf410cc763f0433c8424140e56b2947
John Nnamchi <john.nnamchi@mail.mcgill>
2020-05-08 14:15:04 -0400

    Added run-all bash script

---
+++ run-all.sh
@@ -0,0 +1,20 @@
+echo
+
+echo "1. ARRAYS EXERCISES:"
+echo
+echo "Javascript Arrays"
+node arrays/arrays.js
+echo
+echo "Python Lists"
+python arrays/lists.py
+
+echo
+echo
+
+echo "2. MAPS EXERCISES:"
+echo
+echo "Javascript Objects"
+node maps/objects.js
+echo
+echo "Python Numbers"
+python maps/dictionaries.py
